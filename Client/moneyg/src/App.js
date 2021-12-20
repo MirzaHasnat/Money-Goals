5403836b2a5dc2b9bd8e210316b5834229447d82
mirzahasnat <hasnatmirza111@gmail.com>
2021-12-20 05:52:00 +0000

    [Added] Now user can see goals entries in details [}Some Code Improvement

--- Client/moneyg/src/App.js
+++ Client/moneyg/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Layout } from 'antd';
-import { MoneyCollectOutlined } from '@ant-design/icons';
+import { Layout, Button } from 'antd';
+import { MoneyCollectOutlined, PoweroffOutlined } from '@ant-design/icons';
 import Login from './Components/User/Login'; 
 import Signup from './Components/User/Signup';
 import {ProtectedRoute} from './Components/Routes';
@@ -10,12 +10,24 @@ import Error404 from './Components/Error/404';
 import Home from './Components/Dashboard/Home';
 
 function App() {
+  
+  let logout = ()=>{
+    window.localStorage.removeItem("isAuthorized");
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("goals");
+  }
+
   return (
     <BrowserRouter>
       <Layout style={{minHeight:"100vh"}}>
 
         <Layout.Header>
           <h1 style={{color:"white"}} align="center"> <MoneyCollectOutlined/>MoneyG</h1>
+          {window.localStorage.getItem("isAuthorized") ?
+            <Button type="primary " danger icon={<PoweroffOutlined/>} title='Logout' onClick={()=>{logout()}}></Button>
+          :
+            null
+          }
         </Layout.Header>
 
         <Layout.Content style={{ padding: '0 50px', margin: 20, background:"white" }}>
